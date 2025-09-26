c290ddffed48717485794a2ad61c87bf87515888
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-09-26 14:38:44 +0500

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
   },
-  transpilePackages: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+  transpilePackages: [
+    "@mui/material",
+    "@mui/icons-material",
+    "@emotion/react",
+    "@emotion/styled",
+  ],
 };
 
 export default nextConfig;
